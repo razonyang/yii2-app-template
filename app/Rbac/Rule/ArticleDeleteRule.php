85cfa119a350c2b9617d150ef102a8640ac29c31
razonyang <razonyang@gmail.com>
2019-08-21 18:45:17 +0800

    Add rule

---
+++ app/Rbac/Rule/ArticleDeleteRule.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Rbac\Rule;
+
+use Yii;
+use yii\rbac\Rule;
+
+class ArticleDeleteRule extends Rule
+{
+    public $name = 'articleDelete';
+
+    public function execute($user, $item, $params)
+    {
+        return isset($params['model']) && $user != $params['model']->user_id ? true : false;
+    }
+}
