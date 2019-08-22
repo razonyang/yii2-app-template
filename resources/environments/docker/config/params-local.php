ea9f2ad91abbf9eae323f1e1a4c29027c088e679
razonyang <razonyang@gmail.com>
2019-08-22 16:51:19 +0800

    Update environment configurations

--- resources/environments/docker/config/params-local.php
+++ resources/environments/docker/config/params-local.php
@@ -1,9 +1,6 @@
 <?php
 return [
     'supportEmail' => 'support@example.com',
-
-    // uploader
-    'uploader.host' => 'http://localhost:8080/resources',
     
     'backend.url' => '',
 ];
--- resources/environments/prod/config/params-local.php
+++ resources/environments/prod/config/params-local.php
@@ -2,8 +2,5 @@
 return [
     'supportEmail' => 'support@example.com',
 
-    // uploader
-    'uploader.host' => 'http://localhost:8080/resources',
-    
     'backend.url' => '',
 ];
