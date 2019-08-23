2e0d8b7468adecace012408bbb5e75a3d7f3e177
razonyang <razonyang@gmail.com>
2019-08-23 17:49:55 +0800

    Update about page

--- resources/views/site/about.php
+++ resources/views/site/about.php
@@ -10,3 +10,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 </div>
+
+<ul>
+    <li><strong>Support</strong>: <?= Html::mailto(Yii::$app->params['supportEmail'], Yii::$app->params['supportEmail']) ?></li>
+</ul>
