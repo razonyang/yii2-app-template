c38bf9cdc266130286eab9301c11ea6696b06708
razonyang <razonyang@gmail.com>
2019-08-23 17:39:34 +0800

    Update homepage

--- resources/views/site/index.php
+++ resources/views/site/index.php
@@ -7,7 +7,8 @@ $this->title = 'Home';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Welcome!</h1>
+        <h1>Congratulation!</h1>
     </div>
-    
+    <p class="lead text-center">You have set up Yii2 and Vue application successfully.</p>
+    <p class="text-center"><a class="btn btn-primary btn-lg" href="https://github.com/razonyang/yii2-app-template" role="button">Get started</a></p>
 </div>
