8977f9b745be9aa83ac17d31dedbcf0347b950cb
razonyang <razonyang@gmail.com>
2019-08-29 16:30:07 +0800

    Update login page

--- resources/views/site/login.php
+++ resources/views/site/login.php
@@ -26,6 +26,8 @@ $this->params['breadcrumbs'][] = $this->title;
                 <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
                 <div style="color:#999;margin:1em 0">
+                    No account? <?= Html::a('Create one!', ['/signup']) ?>
+                    <br>
                     If you forgot your password you can <?= Html::a('reset it', ['/request-password-reset']) ?>.
                     <br>
                     Need new verification email? <?= Html::a('Resend', ['/resend-verification-email']) ?>
