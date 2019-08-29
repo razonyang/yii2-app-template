c757246adc4243f606f7703cbcbb7c28458690d1
razonyang <razonyang@gmail.com>
2019-08-29 16:04:14 +0800

    Update Authenticator

--- app/Http/Filter/Auth/Authenticator.php
+++ app/Http/Filter/Auth/Authenticator.php
@@ -5,4 +5,10 @@ use yii\filters\auth\CompositeAuth;
 
 class Authenticator extends CompositeAuth
 {
+    public function authenticate($user, $request, $response)
+    {
+        $identity = $user->getIdentity();
+
+        return $identity ?? parent::authenticate($user, $request, $response);
+    }
 }
