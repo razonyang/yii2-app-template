f27ef993381fa67dcfb1b942c6c2353baa7c7a3e
razonyang <razonyang@gmail.com>
2020-03-06 13:36:15 +0800

    Update about view

--- resources/views/site/about.php
+++ resources/views/site/about.php
@@ -10,7 +10,14 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 </div>
-
 <ul>
-    <li><strong>Support</strong>: <?= Html::mailto(Yii::$app->params['supportEmail'], Yii::$app->params['supportEmail']) ?></li>
+    <li>
+        <a href="https://github.com/razonyang/yii2-app-template" target="_blank">GitHub repository</a>
+    </li>
+    <li>
+        <a href="https://github.com/razonyang/yii2-app-template/issues/new" target="_blank">File an issue or request a new feature</a>
+    </li>
+    <li>
+        <?= Html::mailto('Send me an email', Yii::$app->params['supportEmail']) ?>
+    </li>
 </ul>
