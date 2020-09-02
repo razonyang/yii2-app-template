3ba103c09a80647867373eb13767213774080575
razonyang <razonyang@gmail.com>
2020-09-02 18:14:48 +0800

    Remove dev configs

--- resources/environments/dockerenv/config/bootstrap-local.php
+++ resources/environments/dockerenv/config/bootstrap-local.php
@@ -1,4 +1,4 @@
 <?php
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', ENV_DEVELOPMENT);
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_ENV') or define('YII_ENV', ENV_PRODUCTION);
--- resources/environments/dockerenv/config/web-local.php
+++ resources/environments/dockerenv/config/web-local.php
@@ -16,19 +16,4 @@ $config = [
     ],
 ];
 
-if (YII_ENV_DEV) {
-    $allowedIPs = ['127.0.0.1', '::1'];
-
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = array_merge(require __DIR__ . '/debug.php', [
-        'allowedIPs' => $allowedIPs
-    ]);
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] =  array_merge(require __DIR__ . '/gii.php', [
-        'allowedIPs' => $allowedIPs
-    ]);
-}
-
 return $config;
