e863037073335871dbad944dad57f25e5946a928
razonyang <razonyang@gmail.com>
2020-09-02 18:09:26 +0800

    Fix database config

--- resources/environments/dockerenv/config/common-local.php
+++ resources/environments/dockerenv/config/common-local.php
@@ -2,7 +2,7 @@
 return [
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=' + getenv('DB_HOST') + ';dbname=' + getenv('DB_NAME'),
+            'dsn' => 'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
             'username' => getenv('DB_USER'),
             'password' => getenv('DB_PASSWORD'),
             'enableSchemaCache' => true,
